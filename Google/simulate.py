353f71780a14b6c5bff85493f5086dc19febd5e8
Yingpeng Wang <yp.wangt@gmail.com>
2018-09-08 21:26:48 -0700

    87hbfw5d0sf

--- Google/simulate.py
+++ Google/simulate.py
@@ -1,3 +1,4 @@
+'''
 time = "19:39"
 
 allowed = {int(x) for x in time if x != ':'}
@@ -14,3 +15,35 @@ for block in divmod(cur, 60):
 print(*divmod(cur, 60))
 print(divmod(cur, 60))
 print(allowed)
+'''
+import collections
+from queue import  Queue
+
+n = 5
+edges = [[0,1], [5,2], [2,3], [1,3], [1,4]]
+
+neighbors = collections.defaultdict(list)
+for u, v in edges:
+    neighbors[u].append(v)
+    neighbors[v].append(u)
+
+print(neighbors)
+
+visited = {}
+queue = Queue()
+queue.put(0)
+visited[0] = True
+
+while not queue.empty():
+    
+    cur = queue.get()
+    print(neighbors[cur])
+    visited[cur] = True
+    for node in neighbors[cur]:
+        print(node)
+        if node not in visited:
+            visited[node] = True
+            queue.put(node)
+
+print(queue)
+print(visited)
